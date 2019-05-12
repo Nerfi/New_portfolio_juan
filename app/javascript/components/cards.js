e6ad8ea085881cf8bb0c6b07c04048be58846d3f
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-05-12 18:12:58 +0200

    install yarn packgage with babel to see if I can solve problem with card not working

--- app/javascript/components/cards.js
+++ app/javascript/components/cards.js
@@ -5,3 +5,17 @@ function toggleOpen() {
 }
 
 
+function toggleActive(e) {
+console.log(e.propertyName);
+if (e.propertyName.includes('flex')) {
+this.classList.toggle('open-active');
+
+
+}
+
+
+cards.forEach(card => card.addEventListener('click', toggleOpen));
+cards.forEach(card => card.addEventListener('transitionend', toggleActive));
+
+
+export { toggleOpen };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,8 @@ import "bootstrap";
 
 import { loadDynamicBannerText } from '../components/banner';
 
+import {toggleOpen } from '../components/cards'
 
 loadDynamicBannerText();
+
+toggleOpen();
